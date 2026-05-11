2c15b14e6cde882b0944a6763c2dd893fb219f5e
Hadjer Bouyahiaoui <bouyahiaouihadjer@gmail.com>
2026-05-11 14:44:32 +0200

    Refactor tt_um_example module inputs and unused wires

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_pwm (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -22,7 +22,7 @@ module tt_um_example (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena,  1'b0};
 
 
     reg [7:0] counter;
